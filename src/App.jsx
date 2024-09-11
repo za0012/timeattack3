e0222c3fd3ca6d038bc6d19ae85bf4bad6adaf38
za0012 <zinzeek@gmail.com>
2024-09-11 12:07:42 +0900

    no-refactoring 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [title, setTitle] = useState('');
+    const [view, setView] = useState(0);
+    const queryClient = useQueryClient();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const aaa = async () => {
+        const response = await axios.get('http://localhost:4000/posts');
+        return response.data;
+    };
+
+    const { data, isError, isLoading } = useQuery({
+        queryKey: ['posts'],
+        queryFn: aaa,
+    });
+
+    const addPost = async (newData) => {
+        await axios.post('http://localhost:4000/posts', {
+            title: newData.title,
+            view: newData.view,
+        });
+    };
+
+    const mutation = useMutation({
+        mutationFn: addPost,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['posts']);  //이부분도사실좀참고했다......
+        },
+    });
+
+    if (isLoading) {
+        return <div>로딩중입니다...</div>;
+    }
+
+    if (isError) {
+        return <div>오류가 발생하였습니다...</div>;
+    }
+
+    return (
+        <div>
+            <input value={title} placeholder="title" onChange={(e) => setTitle(e.target.value)} />
+            <input value={view} placeholder="view" onChange={(e) => setView(e.target.value)} />
+            <button
+                onClick={() => {
+                    mutation.mutate({
+                        //여기에 화살표함수 넣었어야한다................. 무섭다,............
+                        title: title,
+                        view: view,
+                    });
+                }}
+            >
+                제출
+            </button>
+            {data.map((post) => {
+                return ( //여기리턴도 좀 참고했다.........
+                    <div>
+                        <p>
+                            {post.title} {post.view}
+                        </p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
-export default App
+export default App;
